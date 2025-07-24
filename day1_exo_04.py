4fb60d2a3efa247db5400bd091e869916ede3345
dukst0n <165058248+dukst0n@users.noreply.github.com>
2025-07-24 15:30:50 +0000

    Add files via upload

---
+++ day1_exo_04.py
@@ -0,0 +1,9 @@
+10 est du type Entier : <class 'int'>
+9,8 est du type Tuple : <class 'tuple'>
+3.14 est du type Flottant : <class 'float'>
+4-4j est du type Complexe : <class 'complex'>
+['Asabeneh', 'Python', 'Finlande'] est du type Liste : <class 'list'>
+Votre nom, votre nom de famille et votre pays sont du type chaîne ou Chaîne de caractère : <class 'str'>
+True est du type Bouléen : <class 'bool'>
+{1, 2, 3, 4} est du type Set: <class 'set'>
+{'nom': 'prénom', 'age': 'Adresse':} est du type Dictionnaire : <class 'dict'>
