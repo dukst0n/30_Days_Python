a4aa4149f9b195ae420c6be9a076de34743b832f
dukst0n <165058248+dukst0n@users.noreply.github.com>
2025-07-24 12:33:19 +0000

    Add files via upload

---
+++ day1_exo_01.py
@@ -0,0 +1,2 @@
+import sys
+print(f"Vous avez installé Python {sys.version}")
---
+++ day1_exo_03.py
@@ -0,0 +1,8 @@
+    # Entrez vos identités
+prenom = input("Votre prénom : ")
+nom = input("Votre nom de famille : ")
+pays = input("Votre pays de provenance : ")
+
+    # Affichage
+print(f"\nJe m'appelle {prenom} {nom}. Je viens de {pays}. Je participe au 30 JOURS de PYTHON.")
+print()
